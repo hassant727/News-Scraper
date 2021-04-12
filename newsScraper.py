cd680c0ce549be5669bec0bb63a070f5466df456
Tariq <hassan.tariq@aptiv.com>
2021-04-12 12:16:21 +0100

    create session and get url and retuurn information back to varible

--- newsScraper.py
+++ newsScraper.py
@@ -1,1 +1,9 @@
+# Write a short script tat takes information from google news and 
+# outputputs to terminal with tile and link
 
+from requets_html import HTMLSession
+# create a session to open up the page
+session = HTMLSession()
+url = "https://news.google.com/topstories?hl=en-IE&gl=IE&ceid=IE:en"
+
+article = session.get(url)
