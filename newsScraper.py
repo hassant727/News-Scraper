03116c5c6939edba3b4868d602284740fcef7f1a
Tariq <hassan.tariq@aptiv.com>
2021-04-12 13:47:08 +0100

    append article info to list to display number of articles with scroll=5

--- newsScraper.py
+++ newsScraper.py
@@ -8,16 +8,19 @@ url = "https://news.google.com/topstories?hl=en-IE&gl=IE&ceid=IE:en"
 
 r = session.get(url)
 
-r.html.render(sleep=1, scrolldown=0)
+r.html.render(sleep=1, scrolldown=5)
 
 articles = r.html.find("article")
 #print(article) -----------------------------
 
+newslist = []
 for item in articles:
     try:
         newsitem = item.find('h3', first=True)
-        title = newsitem.text
-        link = newsitem.absolute_links
-        print(title, link)
+        news_article_info = {"title" : newsitem.text, "link" : newsitem.absolute_links}
+        newslist.append(news_article_info)
+        #print(title, link) -------------------------------
     except:
         pass
+
+print(len(newslist))
