cf38e2f7d1e74aaacdfa738022b760cc5280e51f
Tariq <hassan.tariq@aptiv.com>
2021-04-12 13:12:37 +0100

    loop through the articles to get title and link

--- newsScraper.py
+++ newsScraper.py
@@ -1,9 +1,23 @@
 # Write a short script tat takes information from google news and 
 # outputputs to terminal with tile and link
 
-from requets_html import HTMLSession
+from requests_html import HTMLSession
 # create a session to open up the page
 session = HTMLSession()
 url = "https://news.google.com/topstories?hl=en-IE&gl=IE&ceid=IE:en"
 
-article = session.get(url)
+r = session.get(url)
+
+r.html.render(sleep=1, scrolldown=0)
+
+articles = r.html.find("article")
+#print(article) -----------------------------
+
+for item in articles:
+    try:
+        newsitem = item.find('h3', first=True)
+        title = newsitem.text
+        link = newsitem.absolute_links
+        print(title, link)
+    except:
+        pass
